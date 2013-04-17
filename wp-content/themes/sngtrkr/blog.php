07eed1aeae5cf12840b154bdb07cd91d04f81c06
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-04-17 23:14:08 +0100

    Started frontpage

--- wp-content/themes/sngtrkr/blog.php
+++ wp-content/themes/sngtrkr/blog.php
@@ -4,24 +4,40 @@ Template Name: BlogTemp
 */ 
 ?>
 <?php get_header(); ?>
+<div class="row">
+	<div class="eightcol">
 <?php $wp_query = new WP_Query();
-$wp_query->query('&showposts=5&cat=12,13,14'.'&paged='.$paged);
+$wp_query->query('&showposts=5'.'&paged='.$paged);
 while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-<div class="row blog_posts post_border">
-	<div class="twocol post_info">
-		<a href="<?php the_permalink() ?>"><h4 class="<?php $category = get_the_category(); echo $category[0]->slug; ?>"><?php $category = get_the_category(); echo $category[0]->cat_name; ?></h4></a>
-		<i class="icon-calendar"></i>
-		<p><?php the_time('dS F Y') ?></p>
-		<i class="icon-comments-alt"></i>
-		<p><?php comments_number(); ?></p>
+<div class="row blog_posts">
+	<div class="twelvecol">
+		<div class="row">
+			<div class="threecol last">
+				<?php if (has_post_thumbnail( $post->ID ) ): ?>
+					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+					<img src="<?php echo $image[0]; ?>" />
+				<?php endif; ?>
+			</div>
+			<div class="ninecol last">
+				<a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
+				<p class="large"><i class="icon-calendar"></i><?php the_time('dS F Y') ?></p>
+				<p class="large"><i class="icon-comments-alt"></i><?php comments_number(); ?></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="elevencol">
+				<p><?php the_excerpt(); ?></p>
+			</div>
+		</div>
 	</div>
-	<div class="eightcol post_content">
-		<div class="header2"><img class="star" src="<?php echo bloginfo('template_directory'); ?>/assets/images/star.png"/><a href="<?php the_permalink() ?>"><h2><?php the_title(); ?></h2></a></div>
-		<p><?php the_excerpt(); ?></p>
-	</div>
-	<div class="twocol last tags">
+	<!-- <div class="twocol last tags">
 		<?php $before = ''; the_tags($before, $separator, $after); ?>
-	</div>
+	</div> -->
 </div>
 <?php endwhile;?>
+</div>
+	<div class="fourcol last">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 <?php get_footer(); ?>
--- wp-content/themes/sngtrkr/footer.php
+++ wp-content/themes/sngtrkr/footer.php
@@ -1,10 +1,7 @@
 	</div>
 <footer role="contentinfo">
-
 </footer>
-
 <?php wp_footer(); ?>
-
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-1.9.0.js"%3E%3C/script%3E'))</script>
 <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/application.js"></script>
